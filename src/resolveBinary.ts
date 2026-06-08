61dac9768b418ef331d0e7951fb171fec11c0607
ozymandias724 <kyle.marcy@gmail.com>
2026-06-08 14:39:17 -0400

    gotta patch this so i have a binary for win32-arm

--- src/resolveBinary.ts
+++ src/resolveBinary.ts
@@ -20,10 +20,12 @@ export function resolveAvifencBinary(): string {
     resolvedPath = path.join(binDir, "linux-x64", "avifenc");
   } else if (platform === "win32" && arch === "x64") {
     resolvedPath = path.join(binDir, "win32-x64", "avifenc.exe");
+  } else if (platform === "win32" && arch === "arm64") {
+    // Currently, we only have a Windows x64 binary. If the platform is Windows ARM64, we can attempt to use the x64 binary via emulation.
+    // this is for testing on a vm with Windows ARM64, but in the future, we may want to provide a native ARM64 binary for Windows.
+    return path.join(binDir, "win32-x64", "avifenc.exe");
   } else {
-    throw new Error(
-      `Unsupported platform: ${platform}/${arch}`
-    );
+    throw new Error(`Unsupported platform: ${platform}/${arch}`);
   }
 
   console.log("Resolved avifenc:", resolvedPath);
